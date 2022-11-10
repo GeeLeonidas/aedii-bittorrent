679243eaf837271696111dab887465407a10e30e
Guilherme Leoi <geeleonidas@pm.me>
2022-11-09 21:36:03 -0300

    Add first source files

---
+++ src/peers.py
@@ -0,0 +1,3 @@
+## Decidir a implementação da descoberta: via tracker ou DHT circular?
+## Via tracker parece ser mais simples, porém DHT circular é mais descentralizada
+## Talvez algo entre ambos, um tracker distribuído implementado usando DHT circular
