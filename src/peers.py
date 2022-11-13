1a048782d3970999dcafee19e495870d39fd1f40
Guilherme Leoi <geeleonidas@pm.me>
2022-11-13 02:10:59 -0300

    Add base implementation

--- src/peers.py
+++ src/peers.py
@@ -1,3 +1,37 @@
 ## Decidir a implementação da descoberta: via tracker ou DHT circular?
 ## Via tracker parece ser mais simples, porém DHT circular é mais descentralizada
 ## Talvez algo entre ambos, um tracker distribuído implementado usando DHT circular
+
+import socket as skt
+
+PORT = 33333
+
+## Conecta à DHT
+## Retorna: O id do cliente
+def connect(target_ip: str) -> int:
+    s = skt.socket(skt.AF_INET)
+    s.connect((target_ip, PORT))
+    # TODO: Propagar a divulgação do novo nó
+
+CHUNK_SIZE = 1 << 18 # 2^18 = 262144
+
+## Pega um pedaço do arquivo, tamanho definido por `CHUNK_SIZE`
+def get_chunk(filename: str, idx: int) -> str:
+    with open(filename, mode='r') as file:
+        file.seek(CHUNK_SIZE * idx)
+        return file.read(CHUNK_SIZE)
+
+## Adiciona todos os pedaços de um arquivo à DHT
+## Retorna: Uma lista com os hashes dos pedaços
+def add_file(filename: str) -> list:
+    res = []
+    idx = 0
+    while True:
+        chunk = get_chunk(filename, idx)
+        if chunk == '':
+            break
+        chunk_hash = hash(chunk)
+        # TODO: Inserir o hash do pedaço à DHT (tratando colisões)
+        res.append(chunk_hash)
+        idx += 1
+    return res
